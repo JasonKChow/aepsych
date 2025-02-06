6726036719f757b53aca7cb1f6ea9525dccb561f
Jason Chow <jasonchow@meta.com>
2025-02-05 18:52:53 -0600

    remove ability to modify logging levels
    Summary: This was used inconsistently and causes problems
    
    Test Plan:

--- aepsych/server/server2.py
+++ aepsych/server/server2.py
@@ -26,7 +26,7 @@ from aepsych.server.replay import (
     replay,
 )
 
-logger = utils_logging.getLogger(logging.INFO)
+logger = utils_logging.getLogger()
 
 
 def get_next_filename(folder, fname, ext):
